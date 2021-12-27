10ea8637c192c6db73862a82e821690a733d847a
duce <dptwink@gmail.com>
2021-12-27 23:44:12 +0300

    Add xpos == 4

--- src/main/java/Finaltask2.java
+++ src/main/java/Finaltask2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Finaltask2 {
     public static void main(String[] args) {
+        System.out.println("Запишите уравнение");
         Scanner scan = new Scanner(System.in);
         String input = scan.next();
         char operation = input.charAt(1);
@@ -14,6 +15,10 @@ public class Finaltask2 {
             System.out.println("x равен: " + (Integer.parseInt(input.substring(4)) - Integer.parseInt(input.substring(0, 1))));
         } else if ((xpos == 2) && (operation == '-')) { //4-x=2
             System.out.println("x равен: " + (Integer.parseInt(input.substring(0, 1)) - Integer.parseInt(input.substring(4))));
+        } else if ((xpos == 4) && (operation == '+')) { //4-4=x
+            System.out.println("x равен: " + (Integer.parseInt(input.substring(0, 1)) + Integer.parseInt(input.substring(2, 3))));
+        } else if ((xpos == 4) && (operation == '-')) { //4-4=x
+            System.out.println("x равен: " + (Integer.parseInt(input.substring(0, 1)) - Integer.parseInt(input.substring(2, 3))));
         }
     }
 }
